d54d15f756391c536d5dfc8bf67691bb7e63d883
yabutayukinari <yabutayukinari@gmail.com>
2020-08-17 19:58:55 +0900

    はじめての Django アプリ作成、その3 index書き換え

--- polls/views.py
+++ polls/views.py
@@ -7,11 +7,8 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 # Leave the rest of the views (detail, results, vote) unchanged
 
