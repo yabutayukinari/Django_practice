17b6b0cae5a56be630b5944e792961bb89c83f7f
yabutayukinari <yabutayukinari@gmail.com>
2020-08-18 19:44:31 +0900

    はじめての Django アプリ作成、その3 detail追加

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.template import loader
 
 from .models import Question
@@ -14,7 +14,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
